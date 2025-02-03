0daa96daf834075ddcc776667047b339eb83f7d6
whoami9009 <engineerahad01@gmail.com>
2025-02-03 13:25:06 +0600

    Delete password directory

--- password/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const passwordInput = document.getElementById("email");
-    const showPasswordCheckbox = document.getElementById("showPassword");
-
-    showPasswordCheckbox.addEventListener("click", function () {
-        passwordInput.type = this.checked ? "text" : "password";
-    });
-});
